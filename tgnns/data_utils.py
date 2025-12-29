7e2746003c7d39c0a1f5951995e694b10b396e75
jincheol5 <oscar4293@gmail.com>
2025-12-30 01:47:43 +0900

    update

--- tgnns/data_utils.py
+++ tgnns/data_utils.py
@@ -1,6 +1,7 @@
 import os
 import random
 import pickle
+import networkx as nx
 import torch
 from tqdm import tqdm
 from typing_extensions import Literal
@@ -9,7 +10,7 @@ from .graph_utils import GraphUtils
 class DataUtils:
     dataset_path=os.path.join('..','data','tgnns')
     @staticmethod
-    def save_to_pickle(data,file_name:str,dir_type:Literal['graph','train','val','test'],num_nodes:Literal[20,50,100,500,1000]=20):
+    def save_to_pickle(data,file_name:str,dir_type:Literal['graph','dataset'],num_nodes:Literal[20,50,100,500,1000]=20):
         file_name=file_name+".pkl"
         file_path=os.path.join(DataUtils.dataset_path,dir_type,file_name)
         if dir_type=='test':
@@ -19,7 +20,7 @@ class DataUtils:
         print(f"Save {file_name}")
     
     @staticmethod
-    def load_from_pickle(file_name:str,dir_type:Literal['graph','train','val','test'],num_nodes:Literal[20,50,100,500,1000]=20):
+    def load_from_pickle(file_name:str,dir_type:Literal['graph','dataset'],num_nodes:Literal[20,50,100,500,1000]=20):
         file_name=file_name+".pkl"
         file_path=os.path.join(DataUtils.dataset_path,dir_type,file_name)
         if dir_type=='test':
@@ -28,3 +29,68 @@ class DataUtils:
             data=pickle.load(f)
         print(f"Load {file_name}")
         return data
+
+    @staticmethod
+    def save_graph_to_dataset(graph:nx.DiGraph,src_list:list):
+        """
+        Input:
+            graph: networkx DiGraph
+            src_list: source node list
+        Output:
+            src_list
+            datastream
+            trajectory_list
+        """
+        num_nodes=graph.number_of_nodes()
+        eventstream=GraphUtils.get_eventstream(graph=graph)
+        datastream=GraphUtils.compute_datastream_from_eventstream(eventstream=eventstream,num_nodes=num_nodes) # dict
+        
+        trajectory_list=[]
+        for source_id in src_list:
+            trajectory=GraphUtils.compute_TR_trajectory_from_eventstream(eventstream=eventstream,num_nodes=num_nodes,source_id=source_id) # [E,N,1]
+            trajectory_list.append(trajectory)
+        
+        return src_list,datastream,trajectory_list
+
+    @staticmethod
+    def split_dataset(src_list:list,datastream:dict,trajectory_list:list,train_ratio:float=0.7,val_ratio:float=0.15):
+        """
+        Input:
+        Output:
+            splitted_dataset_dict:
+                train: ()
+                val: ()
+                test: ()
+                sizes: ()
+        """
+        E=datastream['src'].shape[0]
+        train_size=int(E*train_ratio)
+        val_size=int(E*val_ratio)
+        test_size=E-train_size-val_size
+
+        def _slice_ds(ds,start,end):
+            return {
+                'src': ds['src'][start:end],
+                'tar': ds['tar'][start:end],
+                'mem_t': ds['mem_t'][start:end],
+                'emb_t': ds['emb_t'][start:end],
+                'n_mask': ds['n_mask'][start:end]
+            }
+
+        def _slice_trajectories(traj_list,start,end):
+            return [traj[start:end] for traj in traj_list]
+
+        train_ds=_slice_ds(datastream,0,train_size)
+        val_ds=_slice_ds(datastream,train_size,train_size+val_size)
+        test_ds=_slice_ds(datastream,train_size+val_size,E)
+
+        train_traj=_slice_trajectories(trajectory_list,0,train_size)
+        val_traj=_slice_trajectories(trajectory_list,train_size,train_size+val_size)
+        test_traj=_slice_trajectories(trajectory_list,train_size+val_size,E)
+
+        return {
+            'train': (src_list,train_ds,train_traj),
+            'val': (src_list,val_ds,val_traj),
+            'test': (src_list,test_ds,test_traj),
+            'sizes': {'train':train_size,'val':val_size,'test':test_size}
+        }
--- tgnns/graph_utils.py
+++ tgnns/graph_utils.py
@@ -1,6 +1,174 @@
 
+import random
+import numpy as np
 import networkx as nx
 import torch
+from tqdm import tqdm
+
+class GraphGenerator:
+    @staticmethod
+    def remove_self_loop(graph:nx.Graph):
+        graph.remove_edges_from(nx.selfloop_edges(graph))
+
+    @staticmethod
+    def set_edge_time_attr(graph:nx.DiGraph,num_times:int):
+        for src,tar in graph.edges():
+            if src==tar:
+                graph[src][tar]['t']=[1.1]
+            else:
+                timestamp_num=np.random.randint(1,num_times+1)
+                timestamps=np.random.uniform(0.2,1.0,size=timestamp_num)
+                timestamps=np.sort(timestamps).tolist() 
+                graph[src][tar]['t']=timestamps
+    
+    @staticmethod
+    def remove_random_edges(graph:nx.DiGraph,ratio:float=0.3):
+        edges=[(u,v) for u,v in graph.edges() if u!=v]
+        num_edges=len(edges)
+        num_remove=int(num_edges*ratio)
+        remove_edges=random.sample(edges,num_remove)
+        graph.remove_edges_from(remove_edges)
+    
+    @staticmethod
+    def generate_7_type_graphs(num_graphs:int,num_nodes:int,num_times:int):
+        """
+        <<Generate 7-type graphs>>
+        1. ladder graph
+        2. 2D grid graph
+        3. tree graph
+        4. Erdos-Renyi graph
+        5. Barabasi-Albert graph
+        6. 4-community graph
+        7. 4-caveman graph
+        """
+
+        ladder_graph_list=[]
+        grid_graph_list=[]
+        tree_graph_list=[]
+        Erdos_Renyi_graph_list=[]
+        Barabasi_Albert_graph_list=[]
+        community_graph_list=[]
+        caveman_graph_list=[]
+
+        # generate
+        for _ in tqdm(range(num_graphs),desc=f"generate graph..."):
+            """
+            1. generate ladder graph
+            """
+            if num_nodes%2!=0:
+                raise ValueError("ladder graph requires an even number of nodes.")
+            ladder_graph=nx.ladder_graph(num_nodes//2)
+            GraphGenerator.remove_self_loop(graph=ladder_graph)
+            ladder_graph=ladder_graph.to_directed()
+            GraphGenerator.set_edge_time_attr(graph=ladder_graph,num_times=num_times)
+            ladder_graph_list.append(ladder_graph)
+
+            """
+            2. generate 2D grid graph
+            """
+            side_length=int(np.ceil(np.sqrt(num_nodes)))
+            grid_graph=nx.grid_2d_graph(side_length,side_length)
+            grid_graph=nx.convert_node_labels_to_integers(grid_graph)
+            grid_graph=grid_graph.subgraph(range(num_nodes)).copy()
+            GraphGenerator.remove_self_loop(graph=grid_graph)
+            grid_graph=grid_graph.to_directed()
+            GraphGenerator.set_edge_time_attr(graph=grid_graph,num_times=num_times)
+            grid_graph_list.append(grid_graph)
+
+            
+            """
+            3. generate tree graph
+            """
+            tree_graph=nx.random_tree(num_nodes)
+            GraphGenerator.remove_self_loop(graph=tree_graph)
+            tree_graph=tree_graph.to_directed()
+            GraphGenerator.set_edge_time_attr(graph=tree_graph,num_times=num_times)
+            tree_graph_list.append(tree_graph)
+
+            """
+            4. generate Erdos-Renyi graph
+            """
+            p=min(np.log2(num_nodes)/num_nodes,0.5)
+            erdos_renyi_graph=nx.erdos_renyi_graph(num_nodes,p)
+            GraphGenerator.remove_self_loop(graph=erdos_renyi_graph)
+            erdos_renyi_graph=erdos_renyi_graph.to_directed()
+            if num_nodes>=500:
+                GraphGenerator.remove_random_edges(graph=erdos_renyi_graph,ratio=0.7)
+            else:
+                GraphGenerator.remove_random_edges(graph=erdos_renyi_graph,ratio=0.5)
+            GraphGenerator.set_edge_time_attr(graph=erdos_renyi_graph,num_times=num_times)
+            Erdos_Renyi_graph_list.append(erdos_renyi_graph)
+
+            """
+            5. generate Barabasi-Albert graph
+            """
+            if num_nodes<=4:
+                raise ValueError("barabasi_albert graph requires more than 4 number of nodes.")
+            m=random.choice([4,5])
+            barabasi_albert_graph=nx.barabasi_albert_graph(num_nodes,m)
+            GraphGenerator.remove_self_loop(graph=barabasi_albert_graph)
+            barabasi_albert_graph=barabasi_albert_graph.to_directed()
+            GraphGenerator.remove_random_edges(graph=barabasi_albert_graph,ratio=0.7)
+            GraphGenerator.set_edge_time_attr(graph=barabasi_albert_graph,num_times=num_times)
+            Barabasi_Albert_graph_list.append(barabasi_albert_graph)
+            
+            """
+            6. generate 4 community graph
+            """
+            if num_nodes<4:
+                raise ValueError("4-Community graph requires at least 4 nodes.")
+            community_size=num_nodes//4
+            remaining_nodes=num_nodes%4
+            communities=[nx.erdos_renyi_graph(community_size,0.1) for _ in range(4)]
+            community_graph=nx.disjoint_union_all(communities)
+            for i in range(remaining_nodes):
+                community_graph.add_node(community_graph.number_of_nodes())
+            nodes=list(community_graph.nodes())
+            for i in range(len(nodes)):
+                for j in range(i+1,len(nodes)):
+                    if (i//community_size)!=(j//community_size):
+                        if random.random()<0.01:
+                            community_graph.add_edge(i,j)
+            GraphGenerator.remove_self_loop(graph=community_graph)
+            community_graph=community_graph.to_directed()
+            if num_nodes>=500:
+                GraphGenerator.remove_random_edges(graph=community_graph,ratio=0.7)
+            GraphGenerator.set_edge_time_attr(graph=community_graph,num_times=num_times)
+            community_graph_list.append(community_graph)
+        
+            """
+            7. generate 4-caveman graph
+            """
+            if num_nodes<4:
+                raise ValueError("4-Caveman graph requires at least 4 nodes.")
+            clique_size=num_nodes//4
+            remaining_nodes=num_nodes%4
+            caveman_graph=nx.caveman_graph(4,clique_size)
+            for i in range(remaining_nodes):
+                caveman_graph.add_node(caveman_graph.number_of_nodes())
+            edges_to_remove=[edge for edge in caveman_graph.edges() if random.random()<0.8]
+            caveman_graph.remove_edges_from(edges_to_remove)
+            num_shortcuts=int(0.025*num_nodes)
+            for _ in range(num_shortcuts):
+                u,v=random.sample(list(caveman_graph.nodes()),2)
+                if not caveman_graph.has_edge(u,v):
+                    caveman_graph.add_edge(u,v)
+            GraphGenerator.remove_self_loop(graph=caveman_graph)
+            caveman_graph=caveman_graph.to_directed()
+            if num_nodes>=500:
+                GraphGenerator.remove_random_edges(graph=caveman_graph,ratio=0.7)
+            GraphGenerator.set_edge_time_attr(graph=caveman_graph,num_times=num_times)
+            caveman_graph_list.append(caveman_graph)
+
+        graph_list_dict={}
+        graph_list_dict['ladder']=ladder_graph_list
+        graph_list_dict['grid']=grid_graph_list
+        graph_list_dict['tree']=tree_graph_list
+        graph_list_dict['erdos_renyi']=Erdos_Renyi_graph_list
+        graph_list_dict['barabasi_albert']=Barabasi_Albert_graph_list
+        graph_list_dict['community']=community_graph_list
+        graph_list_dict['caveman']=caveman_graph_list
+        return graph_list_dict
 
 class GraphUtils:
     @staticmethod
@@ -124,5 +292,3 @@ class GraphUtils:
             TR_list.append(gamma[:,:1])
         TR_trajectory=torch.stack(TR_list,dim=0) # [E,N,1]
         return TR_trajectory
-    
-    
